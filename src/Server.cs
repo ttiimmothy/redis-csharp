c903b5d569b05ff4c7b51a2ee31f292561a91b65
ttiimmothy <dinosauli2006@gmail.com>
2023-12-05 04:00:13 -0500

    feat: 2nd stage

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,30 @@
+using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
-TcpListener server = new TcpListener(IPAddress.Any, 6379);
-server.Start();
-server.AcceptSocket(); // wait for client
+var server = new TcpListener(IPAddress.Any, 6379);
+try
+{
+  server.Start();
+  while (true)
+  {
+    Console.WriteLine("Waiting for a connection...");
+    using var socket = server.AcceptSocket();
+    Console.WriteLine("Connected!");
+    var buffer = new byte[1024];
+    var bytes = socket.Receive(buffer);
+    var data = Encoding.UTF8.GetString(buffer, 0, bytes);
+    Console.WriteLine("Received: {0}", data);
+    var response = Encoding.UTF8.GetBytes("+PONG\r\n");
+    socket.Send(response);
+  }
+}
+catch (SocketException e)
+{
+  Console.WriteLine(e.Message);
+}
+finally
+{
+  server.Stop();
+}
